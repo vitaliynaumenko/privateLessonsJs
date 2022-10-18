3d8385ee80f70a36523e35f264f987df1e3af1d5
vitaliynaumenko <bambiino89@gmail.com>
2022-10-18 18:18:47 +0300

    homework #1

---
+++ app.js
@@ -0,0 +1,43 @@
+class CalculateSumInArrayOfObject{
+    constructor() {
+        this.obj = {
+                a: {
+                    a1:[1, 2, 3],
+                    a2: [4, false, 8]
+                },
+                b: {
+                    b1: [],
+                    b2: [{}, [], () => {}]
+                },
+                c: { c1: [8, 9, 4] }
+        }
+        this.arr = []
+        this.arrNumbers = []
+    }
+    checkPropertiesOnArray(){
+        for(let i in this.obj){
+            for(let j in this.obj[i]){
+                if (!Array.isArray(this.obj[i][j])) return;
+                this.arr.push(...this.obj[i][j]);
+            }
+        }
+        return this;
+    }
+    checkElementsArrIsNumber(){
+         this.arrNumbers =  [...this.arr.filter(item=> typeof item === 'number')];
+
+        return this;
+    }
+
+    getSumArrayElements(){
+      const sum = this.arrNumbers.reduce((acc, value)=> acc+ value, 0);
+        console.log(sum);
+
+        return this
+    }
+
+}
+new CalculateSumInArrayOfObject()
+    .checkPropertiesOnArray()
+    .checkElementsArrIsNumber()
+    .getSumArrayElements()
