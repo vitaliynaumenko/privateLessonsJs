0d121d0ee91ab1baf10b73e1f5c0c22a5c826f41
vitaliynaumenko <bambiino89@gmail.com>
2022-11-02 18:54:43 +0200

    homework #2: scope & closures changed function

--- lesson_2/index.js
+++ lesson_2/index.js
@@ -1,13 +1,23 @@
-let count = 0;
-function step (f){
-    (function (){
-       count = count+1;
-       f(count)
-})();
+function counter (f){
+    let count = 0;
+     return function (){
+        f(count);
+         displayCount(count);
+         count++
+         return count
+     }
 }
-const f = function (value) {
+
+function displayCount(count) {
+    console.log(count);
+}
+
+const step = counter(f);
+function f (value) {
     return value*2
 }
-step(f);
-step(f);
-step(f);
+step();
+step();
+step();
+
+
